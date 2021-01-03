f6cb0a2d27c97d71e633709d9dd97402b7ffba5b
Guilherme Egoroff Scalassara <guilhermeegoroff@gmail.com>
2021-01-03 15:43:41 -0300

    Inclusao de crud Cat

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './modules/cats.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
